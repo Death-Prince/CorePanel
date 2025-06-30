87827479237cd20aa163a26a5ce18901faf5ea00
Death-Prince <nagaljohnllenard@gmail.com>
2025-06-30 08:23:38 +0800

    Remove DataTable and Chart components along with their associated logic and styles

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,6 @@ import { useEffect } from "react";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { AppSidebar } from "@/components/app-sidebar";
-import { DataTable } from "@/components/data-table";
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 // import { ModeToggle } from "@/components/theme/ModeToggle";
 
@@ -45,9 +43,9 @@ export default function HomePage() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                {/* <ChartAreaInteractive /> */}
               </div>
-              <DataTable data={data} />
+              {/* <DataTable data={data} /> */}
             </div>
           </div>
         </div>
